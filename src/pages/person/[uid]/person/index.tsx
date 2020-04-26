88998abcb3354c2de17dcc16c4c3f7f547d47222
inori-lover <337938188@qq.com>
2020-04-26 19:20:29 +0800

    修正命名

--- src/pages/person/[uid]/person/index.tsx
+++ src/pages/person/[uid]/person/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useRouteMatch, useDispatch, useSelector, PersonModel } from 'umi';
 
-import { PagrParam } from '@/pages/person/[uid]/types';
+import { PageParam } from '@/pages/person/[uid]/types';
 
 export default function PagePerson() {
-  const match = useRouteMatch<PagrParam>();
+  const match = useRouteMatch<PageParam>();
   const uid = match.params.uid;
   const dispatch = useDispatch();
   const {} = useSelector(PersonModel.currentState);

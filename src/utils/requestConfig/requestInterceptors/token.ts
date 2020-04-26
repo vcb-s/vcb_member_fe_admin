c35eb78f1b506603d3102a5a1c8e1fc0623ad909
inori-lover <337938188@qq.com>
2020-04-26 18:21:37 +0800

    um-request暂不支持headers设置

--- src/utils/requestConfig/requestInterceptors/token.ts
+++ src/utils/requestConfig/requestInterceptors/token.ts
@@ -3,10 +3,17 @@ import type { RequestInterceptor } from 'umi-request';
 import { token } from '@/utils/token';
 
 export const tokenInterceptor: RequestInterceptor = function (url, options) {
-  const headers = new Headers(options.headers);
-  headers.append('X-Token', token.token);
-
-  options.headers = headers;
+  if (options.headers) {
+    if (options.headers instanceof Headers) {
+      throw new Error('目前umi-request不支持设定headers为Headers');
+    } else if (Array.isArray(options.headers)) {
+      options.headers.push(['X-Token', token.token]);
+    } else {
+      options.headers['X-Token'] = token.token;
+    }
+  } else {
+    options.headers = { 'X-Token': token.token };
+  }
 
   options.credentials = 'omit';
   return { url, options };

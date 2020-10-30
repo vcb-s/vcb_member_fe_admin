30123c3d8034cc6c00bd2861d18a7b97acd9d5d5
Y <Y@tencent.com>
2020-10-30 15:19:58 +0800

    feat: 在线密码计算工具

---
+++ src/pages/tools/pass-calc/index.tsx
@@ -0,0 +1,80 @@
+import React, { useCallback, useState } from 'react';
+import { Form, Input, Typography } from 'antd';
+import { useAsyncFn, useDebounce } from 'react-use';
+
+const layout = {
+  labelCol: { span: 2 },
+  wrapperCol: { span: 8 },
+};
+const tailLayout = {
+  labelCol: { span: 2 },
+  wrapperCol: { span: 8, offset: 4 },
+};
+
+const PassCalc = React.memo(function PassCalc() {
+  const [pass, setPass] = useState('');
+
+  const [
+    { loading, error: errMsg, value: hash },
+    calc,
+  ] = useAsyncFn(async () => {
+    console.log('1', pass);
+    const { argon2id } = await import('hash-wasm');
+    console.log('2', argon2id);
+    if (!pass) {
+      return '';
+    }
+    const hash = await argon2id({
+      password: pass,
+      salt: '12345678',
+      iterations: 1,
+      parallelism: 1,
+      memorySize: 8,
+      hashLength: 32,
+      outputType: 'encoded',
+    });
+
+    console.log('3', hash);
+
+    return hash;
+  }, [pass]);
+
+  const [isReady, cancel] = useDebounce(calc, 200, [pass]);
+
+  const passChangeHandle = useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => {
+      setPass(evt.target.value);
+    },
+    [],
+  );
+
+  return (
+    <div style={{ padding: 24 }}>
+      <Typography.Title level={3}>计算密码hash</Typography.Title>
+      <Form>
+        <Form.Item {...layout} label='密码'>
+          <Input value={pass} onChange={passChangeHandle} />
+        </Form.Item>
+        <Form.Item
+          {...layout}
+          label='hash'
+          validateStatus={
+            errMsg
+              ? 'error'
+              : !!isReady() && !loading
+              ? 'success'
+              : 'validating'
+          }
+          hasFeedback
+        >
+          <Input value={hash} readOnly />
+        </Form.Item>
+        {/* <Form.Item {...tailLayout}>
+          <Button onClick={() => copyToClipboard(hash || '')}>复制</Button>
+        </Form.Item> */}
+      </Form>
+    </div>
+  );
+});
+
+export default PassCalc;
